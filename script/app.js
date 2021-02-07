56df9c5931515dd217c39d379858db5622ffcd95
Dennis <dennimanadams@gmail.com>
2021-02-07 13:31:49 +0100

    add script to toggle price

---
+++ script/app.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.querySelector('.btn--toggle')
+const basicPrice = document.getElementById('basic-price')
+const profPrice = document.getElementById('prof-price')
+const masterPrice = document.getElementById('master-price')
+
+
+toggleBtn.addEventListener('change', (e) => {
+ if(e.target.checked) {
+    basicPrice.textContent = `$${19.99}`
+    profPrice.textContent = `$${24.99}`
+    masterPrice.textContent = `$${39.99}`
+ } else {
+    basicPrice.textContent = `$${199.99}`
+    profPrice.textContent = `$${249.99}`
+    masterPrice.textContent = `$${399.99}`
+ }
+
+})
